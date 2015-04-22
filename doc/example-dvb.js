f8466db256e2ba25e29f59c333461255d747482a
Jan Snasel <snaselj@gmail.com>
2015-04-22 17:00:08 +0200

    Doc

--- doc/example-dvb.js
+++ doc/example-dvb.js
@@ -2,7 +2,7 @@ Packages["example-dvb"] = {
   "name": "example-dvb",
   "npmname": "ose-example-dvb",
   "caption": "DVB streamer example",
-  "readme": "This example provides a DVB streamer to be used together with an\n[media player example] instance. In this example, a new entry\ncontrolling a dvblast is created.",
+  "readme": "This example provides a DVB streamer to be used together with an\n[media player example] instance. In this example, a new entry\ncontrolling a DVBlast is created.",
   "line": 37,
   "usage": "## Usage\n\nFor the DVB streamer application to work, you need the following\nprerequisities: \n- Node.js and npm \n- DVBlast\n\nTo install the example application, do the following:\n\n    git clone https://github.com/OpenSmartEnvironment/ose-example-dvb\n    cd ose-example-dvb\n    npm install\n\n\nStart the DVB streamer example as follows:\n\n    ./bin/run.js",
   "modules": {

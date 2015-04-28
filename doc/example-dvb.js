b2264a1c14887aaa5bb68a7096e7288671b2a592
Jan Snasel <snaselj@gmail.com>
2015-04-28 12:33:04 +0200

    Doc

--- doc/example-dvb.js
+++ doc/example-dvb.js
@@ -2,9 +2,10 @@ Packages["example-dvb"] = {
   "name": "example-dvb",
   "npmname": "ose-example-dvb",
   "caption": "DVB streamer example",
-  "readme": "This example provides a DVB streamer to be used together with an\n[media player example] instance. In this example, a new entry\ncontrolling a DVBlast is created.",
-  "line": 41,
-  "usage": "## Usage\n\nFor the DVB streamer application to work, you need the following\nprerequisities: \n- Node.js and npm \n- DVBlast\n\nTo install the example application, do the following:\n\n    git clone https://github.com/OpenSmartEnvironment/ose-example-dvb\n    cd ose-example-dvb\n    npm install\n\nConfigure the IP address and port number of the OSE Media player within your network in `bin/run.js`.\n\n    player: 'ws://[ip address]:[port]'\n\n\nStart the DVB streamer example as follows:\n\n    ./bin/run.js",
+  "readme": "This example provides a DVB streamer to be used together with the\n[media player example].",
+  "line": 44,
+  "features": "- streaming using [DVBlast](http://www.videolan.org/projects/dvblast.html)\n- unicast streaming of single channels\n- multicast streaming of all channels in a single multiplex",
+  "usage": "## Usage\n\nFor the DVB streamer application to work, you need the following\nprerequisites: \n- Node.js and npm \n- DVBlast\n\nTo install the example application, do the following:\n\n    git clone https://github.com/OpenSmartEnvironment/ose-example-dvb\n    cd ose-example-dvb\n    npm install\n\nConfigure the IP address and port number of the OSE Media player within your network in `bin/run.js`.\n\n    player: 'ws://[ip address]:[port]'\n\n\nStart the DVB streamer example as follows:\n\n    ./bin/run.js",
   "modules": {
     "bin/run": {
       "name": "bin/run",
--- doc/example-lirc.js
+++ doc/example-lirc.js
@@ -2,8 +2,9 @@ Packages["example-lirc"] = {
   "name": "example-lirc",
   "npmname": "ose-example-lirc",
   "caption": "LIRC example",
-  "readme": "Extends the [media player example]. Allows to control\nthe media player with a IR remote controller via LIRC.\n\nIn this example, the remote controller behaviour is configured as follows:\n- Volume up, down and mute\n- Play, pause, stop\n- Activate the audio command group by pressing \"KEY_AUDIO\" and\n  select a predefined stream by pressing some digit\n- Activate the DVB by pressing \"KEY_TV\" command group and select a\n  predefined DVB channel by pressing some digit\n- Switch to previous or next channel by pressing the \"KEY_PREVIOUS\"\n  and \"KEY_NEXT\" keys.\n\nYou can configure the behaviour to suit your needs.",
-  "line": 54,
+  "readme": "Extends the [media player example]. Allows to control\nthe media player with a IR remote controller via LIRC.",
+  "line": 52,
+  "features": "- Volume up, down and mute\n- Play, pause, stop\n- Activate the audio command group by pressing \"KEY_AUDIO\" and\n  select a predefined stream by pressing some digit\n- Activate the DVB by pressing \"KEY_TV\" command group and select a\n  predefined DVB channel by pressing some digit\n- Switch to previous or next channel by pressing the \"KEY_PREVIOUS\"\n  and \"KEY_NEXT\" keys.",
   "usage": "## Usage\n\nFor the LIRC example to work, you need the following:\n- LIRC-supported hardware\n- Configured and running LIRC daemon\n\nSee [Setting Up LIRC on the\nRaspberryPi](http://alexba.in/blog/2013/01/06/setting-up-lirc-on-the-raspberrypi/)\n\n\nTo install the example application, do the following:\n\n    git clone https://github.com/OpenSmartEnvironment/ose-example-lirc\n    cd ose-example-lirc\n    npm install\n\nConfigure the IP address and port number of the OSE Media player within your network in `bin/run.js`.\n\n    player: 'ws://[ip address]:[port]'\n\nStart the LIRC example as follows:\n\n    ./bin/run.js",
   "modules": {
     "bin/run": {
